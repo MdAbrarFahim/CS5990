1f2240ee9d7803dc84256b1bbb4bb879fb944139
MdAbrarFahim <106899513+MdAbrarFahim@users.noreply.github.com>
2024-03-03 23:05:30 -0800

    Create decision_tree.py

---
+++ decision_tree.py
@@ -0,0 +1,91 @@
+#importing some Python libraries
+from sklearn import tree
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+dataSets = ['cheat_training_1.csv', 'cheat_training_2.csv']
+
+for ds in dataSets:
+    X = []
+    Y = []
+    df = pd.read_csv(ds, sep=',', header=0) #reading a dataset eliminating the header (Pandas library)
+    data_training = np.array(df.values)[:,1:] #creating a training matrix without the id (NumPy library)
+    #transform the original training features to numbers and add them to the 5D array X. For instance, Refund = 1, Single = 1, Divorced = 0, Married = 0,
+    #Taxable Income = 125, so X = [[1, 1, 0, 0, 125], [2, 0, 1, 0, 100], ...]]. The feature Marital Status must be one-hot-encoded and Taxable Income must
+    #be converted to a float.
+    #--> add your Python code here
+    for data in data_training:
+        features = []
+        # Refund
+        if data[0] == 'Yes':
+            features.append(1)
+        else:
+            features.append(0)
+        # Marital Status (One-hot encoding)
+        if data[1] == 'Single':
+            features.extend([1, 0, 0])
+        elif data[1] == 'Divorced':
+            features.extend([0, 1, 0])
+        else:  # Married
+            features.extend([0, 0, 1])
+        # Taxable Income
+        features.append(float(data[2].replace('k', '')))
+        X.append(features)
+        # Class
+        if data[3] == 'Yes':
+            Y.append(1)
+        else:
+            Y.append(2)
+    # Y =
+    #loop your training and test tasks 10 times here
+    for i in range(10):
+        #fitting the decision tree to the data by using Gini index and no max_depth
+        clf = tree.DecisionTreeClassifier(criterion='gini', max_depth=None)
+        clf = clf.fit(X, Y)
+        #plotting the decision tree
+        tree.plot_tree(clf, feature_names=['Refund', 'Single', 'Divorced', 'Married', 'Taxable Income'], class_names=['Yes','No'], filled=True, rounded=True)
+        plt.show()
+        #read the test data and add this data to data_test NumPy
+        #--> add your Python code here
+        df_test = pd.read_csv('cheat_test.csv', sep=',', header=0)
+        data_test = np.array(df_test.values)[:,1:]
+        # data_test =
+        total_accuracy = 0
+        for data in data_test:
+            #transform the features of the test instances to numbers following the same strategy done during training, and then use the decision tree to make the class prediction. For instance:
+            #class_predicted = clf.predict([[1, 0, 1, 0, 115]])[0], where [0] is used to get an integer as the predicted class label so that you can compare it with the true label
+            #--> add your Python code here
+            features = []
+            # Refund
+            if data[0] == 'Yes':
+                features.append(1)
+            else:
+                features.append(0)
+            # Marital Status (One-hot encoding)
+            if data[1] == 'Single':
+                features.extend([1, 0, 0])
+            elif data[1] == 'Divorced':
+                features.extend([0, 1, 0])
+            else:  # Married
+                features.extend([0, 0, 1])
+            # Taxable Income
+            features.append(float(data[2].replace('k', '')))
+            class_predicted = clf.predict([features])[0]
+            #compare the prediction with the true label (located at data[3]) of the test instance to start calculating the model accuracy.
+            #--> add your Python code here
+            if class_predicted == 1 and data[3] == 'Yes':
+                total_accuracy += 1
+            elif class_predicted == 2 and data[3] == 'No':
+                total_accuracy += 1
+        #find the average accuracy of this model during the 10 runs (training and test set)
+        #--> add your Python code here
+        average_accuracy = total_accuracy / len(data_test)
+        #print the accuracy of this model during the 10 runs (training and test set).
+        #your output should be something like that: final accuracy when training on cheat_training_1.csv: 0.2
+        #--> add your Python code here
+        print(f"final accuracy when training on {ds}: {average_accuracy}")
+
+
+    average_accuracy /= len(data_test)
+    print(f"Average accuracy when training on {ds}: {average_accuracy}")
